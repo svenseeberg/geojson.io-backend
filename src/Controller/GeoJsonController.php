62d48ea90cc26af00ad6cf9cce24bb985eb5cae7
Sven Seeberg <mail@sven-seeberg.de>
2022-12-20 18:33:04 +0100

    Fix routes

--- src/Controller/GeoJsonController.php
+++ src/Controller/GeoJsonController.php
@@ -1,53 +1,44 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\GeoJson;
+use App\Entity\GeoJSON;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class GeoJsonController extends AbstractController
 {
-    #[Route('/geojson', name: 'GetGeoJson', method: 'GET')]
+    /**
+    * @Route("/geojson", name="geojson")
+    */
     public function get_geojson(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
 
-        $geojson = new GeoJson();
+        $geojson = new GeoJSON();
 
         $response = new Response();
         $response->setContent(json_encode([
             'data' => 123,
         ]));
         $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
-    #[Route('/geojson', name: 'PutGeoJson', method: 'PUT')]
-    public function put_geojson(ManagerRegistry $doctrine): Response
-    {
-        $entityManager = $doctrine->getManager();
-
-        $geojson = new GeoJson();
-
-        $response = new Response();
-        $response->setContent(json_encode([
-            'data' => 123,
-        ]));
-        $response->headers->set('Content-Type', 'application/json');
-    }
-
-    #[Route('/list', name: 'ListGeoJson')]
+    /**
+    * @Route("/", name="listgeojson")
+    */
     public function list_geojson(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
 
-        $geojson = new GeoJson();
+        $geojson = new GeoJSON();
 
         $response = new Response();
         $response->setContent(json_encode([
             'data' => 123,
         ]));
         $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }
